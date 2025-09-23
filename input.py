e4f3e6d0bf7d03220e328917151841139d7506ad
KISHOR403 <kishorgogoi403@gmail.com>
2025-09-24 00:59:03 +0530

    First commit: added python file

---
+++ input.py
@@ -0,0 +1,105 @@
+# print("Hello World")
+# variable
+
+# name = "kishor"
+# age = 24
+# price = 10.3
+
+# print("My name is", name)
+# print("My age is", age)
+# print("Price is", price)
+
+# SUM
+# a=0
+# b=2
+# c=a+b
+# print("c=", c)
+
+
+# Arithmetic Operators
+# a = 10
+# b = 5
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a ** b)
+
+# Relational Operator
+# a = 10
+# b = 5
+
+# print(a == b)
+# print(a != b)
+# print(a >= b)
+# print(a <= b)
+
+
+#  assinment operator
+# num = 10
+# num -= 5
+# num *= 5
+# num /= 5
+# num %= 5
+# num **= 5
+# print("num :", num)
+
+
+# Type converasion
+# a = 2
+# a = int("2")
+# b = 4.5
+# print(type(a))
+# print(a+b)
+
+
+# input function
+# input("Enter your name: ")
+# name = input("Enter your name:")
+# print("Welcome", name)
+
+# val = input("Enter a number: ")
+# print(type(val), val)
+
+# name = input("Enter your name: ")
+# age = int(input("Enter your age: "))
+# marks = float(input("Enter your marks: "))
+
+# print("Welcome", name)
+# print("Age:", age)
+# print("Marks:", marks)
+
+
+# practice question
+#1. Write a program to input 2 numbers & print their sum
+# first = int(input("Enter first number: "))
+# second = int(input("Enter second number: "))
+
+# print("sum is: ", first + second)
+
+
+# 2. write to input side of a square & print its area
+# side = float(input("Enter side of square: "))
+
+# print("Area of square is: ", side * side)
+
+
+
+# 3. write to input 2 floating point number & print their average
+
+# a = float(input("Enter first number: "))
+# b = float(input("Enter second number: "))
+
+# print("Average: ", (a+b)/2)
+
+
+
+# 4.WAP to input 2 int numbers, a and b.
+# Print True if a is greater than or equal to b. If not print False.
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("statement: ", a >= b)
